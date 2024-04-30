6b63bddc60960c1fef7ccbfea8eb07871b365349
profsamuelrodrigues <55418418+profsamuelrodrigues@users.noreply.github.com>
2024-04-30 18:37:35 -0300

    Conexão com o banco de dados

--- backend/app.js
+++ backend/app.js
@@ -13,9 +13,18 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+//resolver prblemas de cors
+app.use(cors({credentials:true, origin:"http://localhost:3000"}))
+
+//pasta de upload de imagens
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
 //rotas
 const router = require("./routes/Router.js")
 app.use(router)
+
+//Conexão com o banco de dados
+require("./config/db.js")
 
 //rota de teste
 router.get("/", (req, res)=>{
---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+
+const dbUser = process.env.DB_USER
+const dbPassord = process.env.DB_PASSWORD
+//conexão com o banco de dados
+const conn = async ()=>{
+    try {
+        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassord}@cluster0.wmdy9lc.mongodb.net/dbAlunos`)
+        console.log("Conectado ao banco de dados")
+        return dbConn
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+conn()
+
+module.exports = conn
+
+
+
