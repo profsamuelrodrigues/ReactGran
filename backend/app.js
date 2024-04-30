c3f047a5dac9f747b8951c594ed81889a3582866
profsamuelrodrigues <55418418+profsamuelrodrigues@users.noreply.github.com>
2024-04-30 18:15:51 -0300

    Estrutura de pastas

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
+require("dotenv").config()
+
 const express = require("express")
 const path = require("path")
 const cors = require("cors")
 
-const port = 5000
+
+const port = process.env.PORT
 
 const app = express()
 
@@ -10,6 +13,14 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+//rotas
+const router = require("./routes/Router.js")
+app.use(router)
+
+//rota de teste
+router.get("/", (req, res)=>{
+    res.send("Bem vindo!")
+})
 
 app.listen(port, ()=>{
     console.log(`Servidor rodando na porta ${port}`)
