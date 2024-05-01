e57384e90f1488b6ad49821530da29a39ac4979c
profsamuelrodrigues <55418418+profsamuelrodrigues@users.noreply.github.com>
2024-05-01 12:13:40 -0300

    iniciando validações

---
+++ backend/middlewares/userValidations.js
@@ -0,0 +1,36 @@
+const {body} = require('express-validator')
+
+const  userCreateValidation = ()=>{
+    return [
+        body('name')
+            .isString()
+            .withMessage('O nome é obrigatório!')
+            .isLength({min: 3})
+            .withMessage('O nome precisa ter no mínimo 3 caracteres'),
+        body('email')
+            .isString()
+            .withMessage('O email é obrigatório!')
+            .isEmail()
+            .withMessage('Insira um emal válido'),
+        body('password')
+            .isString()
+            .withMessage('A senha é obrigatória!')
+            .isLength({min: 5})
+            .withMessage('A senha precisa ter no mínimo 5 caracteres'),
+        body('confirmPassword')
+            .isString()
+            .withMessage('A confirmação de senha é obrigatória!')
+            .custom((value, {req})=>{
+                if (value != req.body.password) {
+                    throw new Error('As senhas não conferem')
+                }
+
+                return true
+            })
+    ]
+}
+
+module.exports ={
+    userCreateValidation,
+    
+}
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,7 +4,11 @@ const router = express.Router()
 //controller
 const {register} = require('../controllers/UserController.js')
 
+//middlewares
+const validate = require("../middlewares/handleValidation.js")
+const {userCreateValidation} = require("../middlewares/userValidations.js")
+
 //rotas
-router.post('/register', register)
+router.post('/register', userCreateValidation(), validate, register)
 
 module.exports = router
