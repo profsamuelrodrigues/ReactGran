90b39880882ad8b373170fe34b54e137d6fe2073
profsamuelrodrigues <55418418+profsamuelrodrigues@users.noreply.github.com>
2024-05-01 20:41:09 -0300

    finalizando users

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -82,8 +82,67 @@ const getCurrentUser = (req, res)=>{
      res.status(200).json(user)
 }
 
+//atualiza um usuário
+const  update = async(req, res)=>{
+     const {name, password, bio} = req.body 
+     let profileImage = null
+
+     const reqUser = req.user
+     
+     if (req.file) {
+     profileImage = req.file.filename
+     }
+
+     const user = await User.findById(new mongoose.Types.ObjectId(reqUser._id)).select('-password')
+
+     if (name) {
+          user.name = name
+     }
+
+     if (password) {
+          //gera senha hash
+          const salt = await bcrypt.genSalt()
+          const passwordHash = await bcrypt.hash(password, salt)
+          user.password = passwordHash
+     }
+
+     if (profileImage) {
+          user.profileImage = profileImage
+     }
+
+     if (bio) {
+          user.bio = bio
+     } 
+     
+     await user.save()
+
+     res.status(200).json(user)
+}
+
+// busca um usuário pelo id
+const  getUserById = async(req, res)=>{
+     const {id} = req.params
+
+     try {
+          const user = await User.findById(new mongoose.Types.ObjectId(id)).select('-password')
+
+          if(!user) {
+               res.status(404).json({errors:['Usuário não encontrado']}) 
+               return
+          }
+
+          res.status(200).json(user)
+
+     } catch (error) {
+          res.status(404).json({errors:['Usuário não encontrado']}) 
+          return 
+     }
+}
+
 module.exports = {
     register,
     login,
     getCurrentUser,
+    update,
+    getUserById,
 }
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,16 +2,19 @@ const express = require('express')
 const router = express.Router()
 
 //controller
-const {register, login, getCurrentUser} = require('../controllers/UserController.js')
+const {register, login, getCurrentUser, update, getUserById} = require('../controllers/UserController.js')
 
 //middlewares
 const validate = require("../middlewares/handleValidation.js")
-const {userCreateValidation, loginValidation } = require("../middlewares/userValidations.js")
+const {userCreateValidation, loginValidation, userUpdateValidation } = require("../middlewares/userValidations.js")
 const authGuard = require("../middlewares/authGuard.js")
+const { imageUpload } = require('../middlewares/imageUpload.js')
 
 //rotas
 router.post('/register', userCreateValidation(), validate, register)
 router.post('/login', loginValidation(), validate, login)
 router.get('/profile', authGuard, getCurrentUser)
+router.put('/', authGuard, userUpdateValidation(), validate, imageUpload.single("profileImage"), update)
+router.get('/:id', getUserById)
 
 module.exports = router
